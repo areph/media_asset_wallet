b870d9c5fb25217e856dbb4a4c7b5d79c28028c9
areph <40balmung@gmail.com>
2018-04-22 08:46:19 +0900

    コントラクトのデプロイを一度きりにし、abi情報を外だしして読み込めるようにした
    あとローカルのprivate block chainを指定するようにした

---
+++ contents_manager_deploy.py
@@ -0,0 +1,37 @@
+import json
+import web3
+import sys
+import json
+
+from web3 import Web3, HTTPProvider, TestRPCProvider
+from solc import compile_source
+from web3.contract import ConciseContract
+from time import sleep
+
+# Solidity source code
+with open("./ContentsManager.sol", "r") as f:
+    contents_manager_code = f.read()
+
+compiled_sol = compile_source(contents_manager_code) # Compiled source code
+contract_interface = compiled_sol['<stdin>:ContentsManager']
+
+# web3.py instance
+w3 = Web3(HTTPProvider('http://localhost:8545'));
+
+# Instantiate and deploy contract
+contract = w3.eth.contract(abi=contract_interface['abi'], bytecode=contract_interface['bin'])
+
+# Get transaction hash from deployed contract
+tx_hash = contract.deploy(transaction={'from': w3.eth.accounts[0], 'gas': 3000000})
+blockNumber=w3.eth.blockNumber
+sleep(10)
+
+# Get tx receipt to get contract address
+tx_receipt = w3.eth.getTransactionReceipt(tx_hash)
+contract_address = tx_receipt['contractAddress']
+
+# Contract instance in concise mode
+abi = contract_interface['abi']
+
+with open("./contents_manager_deploy.data", "w") as f:
+    f.write(contract_address + "\n" + json.dumps(abi))
--- contents_upload.py
+++ contents_upload.py
@@ -1,33 +1,23 @@
 import json
 import web3
 import sys
+import json
 
 from web3 import Web3, HTTPProvider, TestRPCProvider
 from solc import compile_source
 from web3.contract import ConciseContract
-
-# Solidity source code
-contents_manager_code = open('./ContentsManager.sol').read()
-
-compiled_sol = compile_source(contents_manager_code) # Compiled source code
-contract_interface = compiled_sol['<stdin>:ContentsManager']
+from time import sleep
 
 # web3.py instance
-w3 = Web3(TestRPCProvider())
+w3 = Web3(HTTPProvider('http://localhost:8545'));
 
-# Instantiate and deploy contract
-contract = w3.eth.contract(abi=contract_interface['abi'], bytecode=contract_interface['bin'])
+# Solidity source code
+with open('./contents_manager_deploy.data', "r") as f:
+    deploy_info = f.read().split("\n")
+contract_instance = w3.eth.contract(address=deploy_info[0], abi=json.loads(deploy_info[1]), ContractFactoryClass=ConciseContract)
 
-# Get transaction hash from deployed contract
-tx_hash = contract.deploy(transaction={'from': w3.eth.accounts[0], 'gas': 4000000})
-
-# Get tx receipt to get contract address
-tx_receipt = w3.eth.getTransactionReceipt(tx_hash)
-contract_address = tx_receipt['contractAddress']
-
-# Contract instance in concise mode
-abi = contract_interface['abi']
-contract_instance = w3.eth.contract(address=contract_address, abi=abi,ContractFactoryClass=ConciseContract)
+print(json.loads(deploy_info[1]))
+print(contract_instance)
 
 # uploadConten parameter
 args = sys.argv
@@ -37,4 +27,5 @@ sign = "sign" # TODO 電子署名関数を呼び出す
 amount = 20 #args[3]
 
 contract_instance.uploadContent(content_hash, encryptedContentsUrl, sign, amount, transact={'from': w3.eth.accounts[0]})
+sleep(3)
 print('Contract value: {}'.format(contract_instance.getContentForCreator(content_hash)))
