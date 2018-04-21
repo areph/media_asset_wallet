09668d8650e206fa7d2694ced70ee01ff16a04df
areph <40balmung@gmail.com>
2018-04-21 16:35:32 +0900

    add creator address.

--- ContentsManager.sol
+++ ContentsManager.sol
@@ -3,17 +3,25 @@ contract ContentsManager {
 
   // コンテンツの情報
   struct Content {
-    string contentsUrl;
+    address creator;
+    string encryptedContentsUrl;
+    string digitalSignature;
     uint amount;
   }
   mapping (string => Content) contents;
 
-  function uploadContent(string _contentsHash, string _contentsUrl, uint _amount) public {
-    contents[_contentsHash] = Content({contentsUrl: _contentsUrl, amount: _amount});
+  function uploadContent(string _contentsHash, string _encryptedContentsUrl, string _digitalSignature, uint _amount) public {
+    contents[_contentsHash] = Content({
+      creator: msg.sender,
+      encryptedContentsUrl: _encryptedContentsUrl,
+      digitalSignature: _digitalSignature,
+      amount: _amount
+    });
   }
 
-  function getContent(string _contentsHash) public constant returns (string, uint) {
+  function getContentForCreator(string _contentsHash) public constant returns (string, uint) {
     Content memory content = contents[_contentsHash];
-    return (content.contentsUrl, content.amount);
+    require(msg.sender == content.creator);
+    return (content.encryptedContentsUrl, content.amount);
   }
 }
